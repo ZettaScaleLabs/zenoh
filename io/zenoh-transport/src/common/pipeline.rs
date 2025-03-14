7d2f4eefdaa3c53517dded44f423480296a95a61
Joseph Perez <joperez@hotmail.fr>
2025-03-14 23:39:51 +0100

    deserialize frame lazily

--- io/zenoh-transport/src/common/pipeline.rs
+++ io/zenoh-transport/src/common/pipeline.rs
@@ -981,10 +981,10 @@ mod tests {
         reader::{DidntRead, HasReader},
         ZBuf,
     };
-    use zenoh_codec::{RCodec, Zenoh080};
+    use zenoh_codec::{network::NetworkMessageIter, RCodec, Zenoh080};
     use zenoh_config::{QueueAllocConf, QueueAllocMode};
     use zenoh_protocol::{
-        core::{Bits, CongestionControl, Encoding, Priority},
+        core::{Bits, CongestionControl, Encoding, Priority, Reliability},
         network::{ext, Push},
         transport::{BatchSize, Fragment, Frame, TransportBody, TransportSn},
         zenoh::{PushBody, Put},
@@ -1088,8 +1088,10 @@ mod tests {
                     match res {
                         Ok(msg) => {
                             match msg.body {
-                                TransportBody::Frame(Frame { payload, .. }) => {
-                                    msgs += payload.len()
+                                TransportBody::Frame(Frame { mut payload, .. }) => {
+                                    msgs +=
+                                        NetworkMessageIter::new(Reliability::DEFAULT, &mut payload)
+                                            .count();
                                 }
                                 TransportBody::Fragment(Fragment { more, .. }) => {
                                     fragments += 1;
